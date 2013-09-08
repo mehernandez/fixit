7fc8e2d3668688002380ac148eaf093326b5cc06
fernandohur <fernandohur@gmail.com>
2013-09-08 17:24:06 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fixit::Application.config.secret_key_base = '58b7dc8357efd0bbcf2bd3a4ed05022063172f158ba6ccb27dc1dbf59a3c0b1ba7386d60d588b51f3827e16f6c2d96d5ee7b26335ffe1da1d2a2b5a72b876f62'
